11ef4e6a590a9b7f712c73d1e58601eac19b8ac7
EvanGrace25 <fam@MacBookPro.localdomain>
2024-12-23 05:29:02 -0700

    Run Pnpm create astro@latest

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
-import { defineConfig } from 'astro/config'
+// @ts-check
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://evangrace25.github.io',
-  base: 'writing',
-})
+	site: 'https://example.com',
+	integrations: [mdx(), sitemap()],
+});
--- src/blog/samplepost.md
+++
@@ -1,9 +0,0 @@
-# Testing a header
-
-## Testing header 2
-
-**Testing Bold**
-
-Now this is regular text
-
-~~But this is not regular text~~
